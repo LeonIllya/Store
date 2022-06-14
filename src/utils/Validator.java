8111bb1a7ddcc0ad1080e1cf8a540ad81a9f5bc9
Java-13 <dd0733567@gmail.com>
2022-06-14 14:34:48 +0200

    Add exceptions

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Валидатор ввода значений
@@ -9,24 +10,35 @@ public class Validator {
     public static String validateName(Scanner scanner) {
         String str = scanner.nextLine().trim();
         while (str.isEmpty()) {
-            System.out.print(Contacts.EMPTY_NAME);
-            str = scanner.nextLine().trim();
+            try {
+                str = scanner.nextLine().trim();
+            }catch (InputMismatchException e){
+                System.out.print(Contacts.EMPTY_NAME);
+            }
         }
         return str;
     }
 
     // Валидация ввода количества
     public static int validateQuantityInput(Scanner scanner) {
+
+        String str1 = null;
+
         while (!scanner.hasNextInt()) {
             String str = scanner.nextLine().trim();
             System.out.printf(Contacts.NUM_QUANTITY, str);
         }
+
         int quantity = scanner.nextInt();
+
         while (quantity <= 0) {
             System.out.println(Contacts.INCORRECT_QUANTITY);
             while (!scanner.hasNextInt()) {
-                String str = scanner.next().trim();
-                System.out.printf(Contacts.NUM_QUANTITY, str);
+                try{
+                    str1 = scanner.next().trim();
+                } catch (InputMismatchException e){
+                    System.out.printf(Contacts.NUM_QUANTITY, str1);
+                }
             }
             quantity = scanner.nextInt();
         }
@@ -35,16 +47,24 @@ public class Validator {
 
     // Валидация ввода цены
     public static double validatePriceInput(Scanner scanner) {
+
+        String str1 = null;
+
         while (!scanner.hasNextDouble()) {
             String str = scanner.nextLine().trim();
             System.out.printf(Contacts.PRICE, str);
         }
+
         double price = scanner.nextDouble();
+
         while (price <= 0) {
             System.out.print(Contacts.INCORRECT_PRICE);
             while (!scanner.hasNextDouble()) {
-                String str = scanner.nextLine().trim();
-                System.out.printf(Contacts.PRICE, str);
+                try{
+                    str1 = scanner.nextLine().trim();
+                } catch (InputMismatchException e){
+                    System.out.printf(Contacts.PRICE, str1);
+                }
             }
             price = scanner.nextDouble();
         }
